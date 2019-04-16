6f4fd9b90bb35bc3f492edbbf4d01c736ff26e1d
Anna <piladzisi@gmail.com>
2019-04-16 16:48:00 +0300

    current weather struct

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by anna.sibirtseva on 16/04/2019.
+//  Copyright © 2019 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipProbability: Double
+    let summary: String
+    let icon: String
+}
