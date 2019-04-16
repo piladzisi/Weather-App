5046710840da9f524681b0b3b2ae2b97c8ba7800
Anna <piladzisi@gmail.com>
2019-04-16 17:06:37 +0300

    icon switch cases

--- Stormy/CurrentWeather.swift
+++ Stormy/CurrentWeather.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct CurrentWeather {
     let temperature: Double
@@ -14,4 +15,23 @@ struct CurrentWeather {
     let precipProbability: Double
     let summary: String
     let icon: String
+}
+
+extension CurrentWeather {
+    var iconImage: UIImage {
+        //return UIImage(imageLiteralResourceName: icon) will crash in case of tornado
+        switch icon {
+        case "clear-day": return #imageLiteral (ResourceName: "clear-day")
+        case "clear-night": return #imageLiteral(resourceName: "clear-night")
+        case "rain": return #imageLiteral(resourceName: "rain")
+        case "snow": return #imageLiteral(resourceName: "snow")
+        case "sleet": return #imageLiteral(resourceName: "sleet")
+        case "wind": return #imageLiteral(resourceName: "wind")
+        case "fog": return #imageLiteral(resourceName: "fog")
+        case "cloudy": return #imageLiteral(resourceName: "cloudy")
+        case "partly-cloudy-day": return #imageLiteral(resourceName: "partly-cloudy-day")
+        case "partly-clody-night": return #imageLiteral(resourceName: "partly-cloudy-night")
+        default: return #imageLiteral(resourceName: "default")
+       }
+    }
 }
